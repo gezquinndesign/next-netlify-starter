30baedbc1ed5639035155206ff357565f0f4653e
Gez Quinn <holla@gezquinn.design>
2024-10-05 21:15:49 +0100

    data

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,24 +1,24 @@
 export default function Recipe({ title, recipe }) {
   return (
     <div vocab="https://schema.org/" typeof="Recipe">
-      <h1 data-property="name">{title}</h1>
-      <span data-property="author">Gez Quinn</span>,
-      <span data-property="recipeYield">1 serving</span>
-      <h3 data-property="recipeIngredientsHeader">Ingredients</h3>
+      <h1 property="name">{title}</h1>
+      <span property="author">Gez Quinn</span>,
+      <span property="recipeYield">1 serving</span>
+      <h3 property="recipeIngredientsHeader">Ingredients</h3>
       <div className="wprm-recipe-ingredient-group">
-        <ul data-property="recipeIngredients">
+        <ul property="recipeIngredients">
           {recipe.split("\n").map((line, index) => (
-            <li data-property="recipeIngredient" key={index}>
+            <li property="recipeIngredient" key={index}>
               {line}
             </li>
           ))}
         </ul>
       </div>
-      <h3 data-property="recipeIngredientsHeader">Instructions</h3>
-      <span data-property="recipeInstructions">
+      <h3 property="recipeIngredientsHeader">Instructions</h3>
+      <span property="recipeInstructions">
         Blend greens and liquid until smooth.
       </span>
-      <span data-property="recipeInstructions">
+      <span property="recipeInstructions">
         Add remaining ingredients and blend again until smooth.
       </span>
     </div>
