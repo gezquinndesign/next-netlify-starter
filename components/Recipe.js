ce24776cd2fd0f0e6277b68789677deb2333c749
Gez Quinn <holla@gezquinn.design>
2024-10-05 22:28:17 +0100

    Refactor Recipe component to update image URL and include structured data

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,32 +1,20 @@
 import Head from "next/head";
+import { RecipeJsonLd } from 'next-seo';
 
 export default function Recipe({ title, recipe }) {
   return (
     <>
-      <Head>
-        <script type="application/ld+json">
-          {`
-      {
-        "@context": "https://schema.org/",
-        "@type": "Recipe",
-        "name": "${title}",
-        "author": "Gez Quinn",
-        "image": "https://friendly-cheesecake-e6101a.netlify.app/green-smoothie.jpg",
-        "recipeYield": "1 serving",
-        "recipeIngredient": [
-          ${recipe
-            .split("\n")
-            .map((line, index) => `"${line}"`)
-            .join(",\n")}
-        ],
-        "recipeInstructions": [
+      <RecipeJsonLd
+        name={title}
+        authorName="Gez Quinn"
+        image="https://friendly-cheesecake-e6101a.netlify.app/green-smoothie.jpg"
+        yields="1"
+        ingredients={recipe.split("\n")}
+        instructions={[
           "Blend greens and liquid until smooth.",
-          "Add remaining ingredients and blend again until smooth."
-        ]
-      }
-      `}
-        </script>
-      </Head>
+          "Add remaining ingredients and blend again until smooth.",
+        ]}
+      />
       <div vocab="https://schema.org/" typeof="Recipe">
         <img property="image" src="/green-smoothie.jpg" alt="Green Smoothie" />
         <h1 property="name">{title}</h1>
