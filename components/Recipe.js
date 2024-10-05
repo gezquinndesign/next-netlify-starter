060ccba3b18bf0706411f746c248edc1334e938c
Gez Quinn <holla@gezquinn.design>
2024-10-05 21:09:23 +0100

    fix: typos

--- components/Recipe.js
+++ components/Recipe.js
@@ -2,24 +2,11 @@ export default function Recipe({ title, recipe }) {
   return (
     <div vocab="https://schema.org/" typeof="Recipe">
       <h1 data-property="name">{title}</h1>
-      By <span data-property="author">Gez Quinn</span>,
+      <span data-property="author">Gez Quinn</span>,
       <meta data-property="datePublished" content="2024-10-05">
         May 8, 2009
       </meta>
-      {/* <img data-property="image" src="bananabread.jpg"
-  alt="Banana bread on a plate" /> */}
-      {/* <span data-property="description">This classic banana bread recipe comes
-from my mom -- the walnuts add a nice texture and flavor to the banana
-bread.</span> */}
-      {/* Prep Time: <meta data-property="prepTime" content="PT15M">15 minutes
-Cook time: <meta data-property="cookTime" content="PT1H">1 hour */}
-      Yield: <span data-property="recipeYield">1 serving</span>
-      {/* Tags: <link data-property="suitableForDiet" href="https://schema.org/LowFatDiet" />Low Fat */}
-      {/* <div data-property="nutrition" typeof="NutritionInformation">
-  Nutrition facts:
-  <span data-property="calories">240 calories</span>,
-  <span data-property="fatContent">9 grams fat</span>
-</div> */}
+      <span data-property="recipeYield">1 serving</span>
       <h3 data-property="recipeIngredientsHeader">Ingredients</h3>
       <div className="wprm-recipe-ingredient-group">
         <ul data-property="recipeIngredients">
@@ -30,9 +17,9 @@ Cook time: <meta data-property="cookTime" content="PT1H">1 hour */}
           ))}
         </ul>
       </div>
-      <h3 data-property="recipeIngredientsHeader">Instrunctions</h3>
+      <h3 data-property="recipeIngredientsHeader">Instructions</h3>
       <span data-property="recipeInstructions">
-        Blend greens and liquid until smooth.
+        Blend greens and liquid until smooth.
       </span>
       <span data-property="recipeInstructions">
         Add remaining ingredients and blend again until smooth.
