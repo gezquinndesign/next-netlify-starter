2e9f8b4e799a2874211f0a25e8d38bbac61cc6ed
Gez Quinn <holla@gezquinn.design>
2024-10-05 19:54:17 +0100

    feat: cpus

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify: true,
+  swcMinify: true,
   staticPageGenerationTimeout: 1000,
+  experimental: {
+    cpus: 1,
+  },
 };
 
 module.exports = nextConfig;
