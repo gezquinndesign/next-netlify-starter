24b27011fe36eaf5f09838ec5a1914fd6992f68e
Gez Quinn <holla@gezquinn.design>
2024-10-08 13:49:03 +0100

    Refactor RecipeApp component to update initial index state and add navigation buttons

--- components/RecipeApp.js
+++ components/RecipeApp.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Recipe from './Recipe'
 
-export default function RecipeApp ({recipes, slug = 'cherry-on-top'}) {
-  console.log('slug', slug, recipes)
-  const recipe = recipes.find((recipe) => recipe.slug === slug)
+export default function RecipeApp ({recipes, slug}) {
+  const [index, setIndex] = React.useState(6)
+  const recipe = recipes[index]
+
+  const previousRecipe = () => {
+    setIndex(currentIndex => (currentIndex - 1) % recipes.length)
+  }
+  const nextRecipe = () => {
+    setIndex(currentIndex => (currentIndex + 1) % recipes.length)
+  }
+
   return (
     <div>
       <Recipe {...recipe} />
+      <button disabled={index===0} onClick={previousRecipe}>Previous Recipe</button>
+      <button disabled={index===recipes.length - 1} onClick={nextRecipe}>Next Recipe</button>
     </div>
   )
 }
